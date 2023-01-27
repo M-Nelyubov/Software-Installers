f457a68e078b8ebe623e655a6c2872b161343e43
M-Nelyubov <54564357+M-Nelyubov@users.noreply.github.com>
2023-01-26 20:27:35 -0500

    install success validation and Env:Path update

--- applications/install-python.ps1
+++ applications/install-python.ps1
@@ -28,9 +28,17 @@ Write-Host "Installing Python $currentVersion"
 # Download the .exe for the latest version of Python
 $downloadUrl = "https://www.python.org/ftp/python/$currentVersion/python-$currentVersion-amd64.exe"
 $installerPath = "C:\Users\$env:username\AppData\Local\Temp\python-$currentVersion.exe"
-Write-Host ($downloadUrl, $installerPath)
 $webClient.DownloadFile($downloadUrl, $installerPath)
 
 # Silently install the downloaded Python exe
 # Arguments from: https://silentinstallhq.com/python-3-10-silent-install-how-to-guide/
 Start-Process -FilePath $installerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+
+# Verify installation was successful
+$currentlyInstalled = "HKLM:\SOFTWARE\Python\PythonCore\" | where {Test-Path $_} | foreach {ls $_} | foreach {$_.GetValue('Version')}
+if($currentVersion -in $currentlyInstalled){
+    Write-Host -ForegroundColor Green "Latest version has been installed: Python - $currentVersion"
+
+	# Refresh Environment Variable
+	$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
