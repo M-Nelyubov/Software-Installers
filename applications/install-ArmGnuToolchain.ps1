58bd2781245ee12f722fd4380e34285ea738392d
M-Nelyubov <54564357+M-Nelyubov@users.noreply.github.com>
2023-02-22 19:05:04 -0500

    added to path environment variable

--- applications/install-ArmGnuToolchain.ps1
+++ applications/install-ArmGnuToolchain.ps1
@@ -16,6 +16,7 @@
     
 #>
 $softwareName = "Arm GNU Toolchain"
+$installationDir = "C:\Program Files (x86)\Arm GNU Toolchain arm-none-eabi"
 
 # Returns an object containing the current version and download URL for the software
 Function get-LatestVersionData {
@@ -40,11 +41,11 @@ Function get-InstalledVersion {
     <#
         Returns the version data if the software is installed, and no value otherwise
     #>
-    @("C:\Program Files (x86)\Arm GNU Toolchain arm-none-eabi" | where {Test-Path $_} | ls | sort -Property LastWriteTime | foreach {$_.Name})[0]
+    @($installationDir | where {Test-Path $_} | ls | sort -Property LastWriteTime | foreach {$_.Name})[0]
 }
 
 Function get-upToDateStatus {
-    (get-LatestVersionData).currentVersion -in (get-InstalledVersion).Trim().Replace(" ",".")
+    ((get-LatestVersionData).currentVersion -in (get-InstalledVersion).Trim().Replace(" ",".")) -and ($env:Path -like "*$installationDir*")
 }
 
 Function install-LatestVersion {
@@ -53,6 +54,20 @@ Function install-LatestVersion {
     $webClient.DownloadFile($downloadUrl, $webPageFilePath)
 
     Start-Process -FilePath $webPageFilePath -ArgumentList "/S" -Wait
+
+    # Add path to environment variable
+    $basePath = get-InstalledVersion
+    $newAddition = ";$basePath\bin"
+    $prior = [Environment]::GetEnvironmentVariable('Path')
+    if(-not ($newAddition -in $prior)){
+        # Remove any old installs from the environment variable
+        $allVersionsRoot = $installationDir
+        $prior = @($prior.Split(";") | where {-not ($allVersionsRoot -in $_)}) -join ";"
+
+        # save new environment path
+        [Environment]::SetEnvironmentVariable('Path', $prior + $newAddition, 'Machine')
+    
+    }
 }
 
 
@@ -74,13 +89,12 @@ if(get-upToDateStatus){
 
 Write-Host "Installing $softwareName $currentVersion"
 install-LatestVersion -currentVersion $currentVersion -downloadUrl $latestData.downloadUrl
+# Refresh Environment Variable after the install
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
 # Verify installation was successful
 if(get-upToDateStatus){
     Write-Host -ForegroundColor Green "Latest version has been installed: $softwareName - $currentVersion"
-
-	# Refresh Environment Variable after the install
-	$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 } else {
     Write-Error "Failed to install latest version of $softwareName - $currentVersion"
 }
